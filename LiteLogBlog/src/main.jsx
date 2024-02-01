42a6b4ca49ede9dd4f6e69b209329e0cb2402e68
g-nesh <ganeshnimbare25@gmail.com>
2024-02-01 22:52:14 +0530

    solved some debugging issue

--- LiteLogBlog/src/main.jsx
+++ LiteLogBlog/src/main.jsx
@@ -4,7 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx"
+import { AuthLayout, Login } from "./components/index.js";
+
+import AddPost from  './pages/AddPost';
+import Signup from "./pages/Signup.jsx";
+import EditPost from "./pages/EditPost.jsx"
+
+import Post from "./pages/Post.jsx";
+
+import AllPosts from  "./pages/AllPosts.jsx";
 
 const router = createBrowserRouter([
   {
